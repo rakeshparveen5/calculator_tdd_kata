a0a27e1fd97ccc3d2ce442df583f0e91d34d9f50
rakeshparveen5 <rakesh.parveen5@gmail.com>
2026-01-13 16:34:50 +0530

    REFACTOR: Added docs & added empty string check

--- lib/calculator_tdd_kata.dart
+++ lib/calculator_tdd_kata.dart
@@ -1,5 +1,10 @@
+// A class that performs addition operations on the input of numbers of Type String
 class StringCalculator {
+  // Method that performs addition on numbers
   int add(String numbers) {
+    if (numbers.isEmpty) {
+      return 0;
+    }
     return 0;
   }
 }
