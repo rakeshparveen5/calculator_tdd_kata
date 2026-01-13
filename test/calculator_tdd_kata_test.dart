8d23ba9dc6a92518e9883ee4b1555f8a12e52ce8
rakeshparveen5 <rakesh.parveen5@gmail.com>
2026-01-13 17:09:14 +0530

    RED: test returns sum having other delimiters with prefix double slash

--- test/calculator_tdd_kata_test.dart
+++ test/calculator_tdd_kata_test.dart
@@ -26,4 +26,9 @@ void main() {
     StringCalculator calculator = StringCalculator();
     expect(calculator.add('1\n2,3'), equals(6));
   });
+
+  test('handles other delimiters with prefix // in numbers', () {
+    StringCalculator calculator = StringCalculator();
+    expect(calculator.add('//;\n1;2'), equals(3));
+  });
 }
